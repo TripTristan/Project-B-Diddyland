387400309726e7beb458d50a7df9eabb9b87123f
TripTristan <t.t.mannee@gmail.com>
2025-10-07 15:21:04 +0200

    Base code for Acces and Model files

---
+++ ProjectB/DataAccess/ResMenu.cs
@@ -0,0 +1,26 @@
+using Microsoft.Data.Sqlite;
+using Dapper;
+
+public class ResMenuAccess
+{
+    private readonly SqliteConnection _connection = new($"Data Source=DataSources/project.db");
+
+    public void Link(ResMenuModel resMenu)
+    {
+        const string sql = @"INSERT INTO ResMenu (ReservationID, MenuID)
+                             VALUES (@ReservationID, @MenuID)";
+        _connection.Execute(sql, resMenu);
+    }
+
+    public IEnumerable<ResMenuModel> GetAll()
+    {
+        const string sql = "SELECT * FROM ResMenu";
+        return _connection.Query<ResMenuModel>(sql);
+    }
+
+    public void Delete(int id)
+    {
+        const string sql = "DELETE FROM ResMenu WHERE ID = @ID";
+        _connection.Execute(sql, new { ID = id });
+    }
+}
