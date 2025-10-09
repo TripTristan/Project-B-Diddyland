e9261b6abef78c062c3175b710fbd81584d498c4
oRitesh <ramai@outlook.com>
2025-10-09 14:41:28 +0200

    MenuLogic - AddItem

--- ProjectB/Logic/MenuLogic.cs
+++ ProjectB/Logic/MenuLogic.cs
@@ -1,4 +1,35 @@
 public class MenuLogic
 {
-    
+    private readonly MenusAccess _access;
+
+    public MenuLogic(MenusAccess access)
+    {
+        _access = access;
+    }
+
+    public IEnumerable<MenuModel> GetAll() => _access.GetAll();
+
+    public void AddItem(string? food, string? drink, double price)
+    {
+        food ??= "";        // Set Defaullt Value
+        drink ??= "";       
+
+        if (string.IsNullOrWhiteSpace(food) && string.IsNullOrWhiteSpace(drink))
+            throw new ArgumentException("Provide at least a food or a drink name.");
+        if (price < 0) throw new ArgumentException("Price cannot be negative.");
+
+        var model = new MenuModel
+        {
+            Food = food.Trim(),
+            Drink = drink.Trim(),
+            Price = price
+        };
+
+        _access.Insert(model);
+    }
+
+
+    public void AddFood(string name, double price) => AddItem(name, "", price);
+    public void AddDrink(string name, double price) => AddItem("", name, price);
+
 }
