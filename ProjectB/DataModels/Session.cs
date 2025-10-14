fd0a9507bd85ffc9c47f3fb2aedbcd1af6368c6c
Z.Laptop.Thinkpad <gz980113@gmail.com>
2025-10-14 08:17:04 +0200

    Creat: --Session.cs (model) --SessionAccess.cs

---
+++ ProjectB/DataModels/Session.cs
@@ -0,0 +1,8 @@
+public class Session
+{
+    public int Id { get; set; }
+    public string Date { get; set; } = "";
+    public string Time { get; set; } = "";
+    public int MaxCapacity { get; set; }
+    public int CurrentBookings { get; set; }
+}
