582d45b4fb66cc4a4a0f389b0ef72889599957ce
Z.Laptop.Thinkpad <gz980113@gmail.com>
2025-11-13 01:47:58 +0100

    OfferRule Model Update

--- ProjectB/DataModels/OfferRuleModel.cs
+++ ProjectB/DataModels/OfferRuleModel.cs
@@ -2,6 +2,28 @@ public class OfferRuleModel
 {
     public int Id { get; set; }
     public int OfferId { get; set; }
-    public string RuleType { get; set; } = "Quantity";
-    public int RuleValue { get; set; } // 
+    public string RuleType { get; set; } = ""; 
+    public string RuleValue { get; set; } = "";
+    public string Description { get; set; } = "";
+
+    // RuleValue te convert to object
+    // RuleType = "Quantity", IntValue 
+    // RuleType = "PromoCode", StringValue
+    // RuleType = "Birthday", DateTimeValue
+    public T GetRuleValue<T>() where T : class
+    {
+        try
+        {
+            return JsonConvert.DeserializeObject<T>(RuleValue);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public void SetRuleValue<T>(T value)
+    {
+        RuleValue = JsonConvert.SerializeObject(value);
+    }
 }
